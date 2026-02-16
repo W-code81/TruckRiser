77e53239af5f9057f2970996ec92d2115368f7ea
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-02-16 23:21:02 +0100

    impleamented flash and cookieparser

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const e = require("connect-flash");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -6,6 +5,9 @@ require("dotenv").config();
 const validator = require("validator");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
 const mongoose = require("mongoose");
 
 mongoose
@@ -42,11 +44,27 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.use(flash()); //flash middleware
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success"); // global success variable
+  res.locals.error = req.flash("error"); // global error variable
+  next();
+});
+
+app.use(cookieParser()); //cookie parser middleware
+app.use((req, res, next) => {
+  res.locals.cookieConsent = req.cookies.cookieConsent; //global variable for accessing cookies
+  next();
+});
+
+
+
 app.get("/", (req, res) => {
   res.redirect("/signup");
 });
 
-app.get("/home", (req, res) => { //make protected 
+app.get("/home", (req, res) => {
+  //make protected
   res.render("index");
 });
 
@@ -58,18 +76,23 @@ app
   .post(async (req, res) => {
     try {
       const { email, password } = req.body;
-      const registeredUser = await User.findOne({ email: email });
+      const registeredUser = await User.findOne({ email: email }); //get user by the email
 
       if (!registeredUser) {
-       return res.status(401).send("User not found");
+        req.flash("error", "user not found");
+        res.redirect("/login");
+
+        //  return res.status(401).send("User not found");
       }
 
       const match = await bcrypt.compare(password, registeredUser.password);
 
       if (match) {
+        req.flash("success", "sucecessfully login!");
         res.redirect("/home");
       } else {
-        res.status(401).send("Invalid password");
+        req.flash("error", "failed to login!");
+        res.redirect("/login");
       }
     } catch (error) {
       res.status(500).send("Login error");
@@ -88,7 +111,8 @@ app
       const { email, password } = req.body;
 
       if (!email || !password) {
-        res.status(400).send("Please Specify credentials"); // bad request, a flash will be here tho
+        req.flash("error", "please specify credentials");
+        // res.status(400).send("Please Specify credentials"); // bad request, a flash will be here tho
         return;
       }
 
@@ -98,20 +122,29 @@ app
         email: email,
         password: hashedPassword,
       });
-
+      req.flash("success", "successfully signed in");
       res.redirect("/home");
     } catch (err) {
       if (err.code === 11000) {
-        res.status(500).send("User already registered");
+        req.flash("error", "User already registered");
+        // res.status(500).send("User already registered"); //send and redirect can't work at the same time fix this
         return res.redirect("/signup");
       }
-      res.status(500).send("Sign up error");
+      req.flash("error", "sign up error");
+      // res.status(500).send("Sign up error");
       console.error("sign up error: ", err);
     }
   });
 
-app.get("/logout", (req, res) => { 
+app.get("/logout", (req, res) => {});
 
+app.post("/accept-cookies", (req, res) => {
+  (res.cookie("cookieConsent", "true"),
+    {
+      httpOnly: false, //allows client side js to access the cookie since we need to check cookie consent in the frontend
+      maxAge: 1000 * 60 * 60 * 24, //expires after a day
+    });
+  res.status(200);
 });
 
 app.listen(port, () => {
