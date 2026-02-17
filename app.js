e9dde363696e9c9975771bb77e97f25aab04f08a
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-02-17 01:15:45 +0100

    added sessions,cookies and cleaned up the code structure

--- app.js
+++ app.js
@@ -3,13 +3,56 @@ const app = express();
 const port = process.env.PORT || 3000;
 require("dotenv").config();
 const validator = require("validator");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
 const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose").default;
 const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
 const mongoose = require("mongoose");
 
+
+// MIDDLEWARES AND INITIALIZATIONS
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+// EXPRESS SESSIONS
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, // doesn't save unchanged sessions
+    saveUninitialized: false, // doesn't create if empty
+    cookie: {
+      httpOnly: true, //prevents client side js from accessing the cookie
+      maxAge: 1000 * 60 * 60 * 24, //cookie expires after 1 day
+    },
+  }),
+);
+
+// PASSPORT INITIALIZATION AND SESSIONS 
+app.use(passport.initialize());
+app.use(passport.session()); //persists sessions
+
+
+// FLASH 
+app.use(flash()); 
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success"); // global success variable
+  res.locals.error = req.flash("error"); // global error variable
+  next();
+});
+
+
+// COOKIEPARSER
+app.use(cookieParser());
+app.use((req, res, next) => {
+  res.locals.cookieConsent = req.cookies.cookieConsent; //global variable for accessing cookies
+  next();
+});
+
+
+// MONGODB INITIALIZATION AND SCHEMA
 mongoose
   .connect(process.env.MONGO_LOCAL_URI)
   .then(() => console.log("Connected to MongoDB"))
@@ -38,34 +81,29 @@ const userSchema = new mongoose.Schema(
   },
 );
 
+userSchema.plugin(passportLocalMongoose, {usernameField: "email"}) //uses local auth strategies (hash,salt,auth middleware)
 const User = mongoose.model("User", userSchema);
 
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
-app.use(flash()); //flash middleware
-app.use((req, res, next) => {
-  res.locals.success = req.flash("success"); // global success variable
-  res.locals.error = req.flash("error"); // global error variable
-  next();
-});
-
-app.use(cookieParser()); //cookie parser middleware
-app.use((req, res, next) => {
-  res.locals.cookieConsent = req.cookies.cookieConsent; //global variable for accessing cookies
-  next();
-});
+// PASSPORT LOCAL STRATEGY
+passport.use(User.createStrategy()); //local strategy
+passport.serializeUser(User.serializeUser()); //creates cookies
+passport.deserializeUser(User.deserializeUser()); //retrieves cookie info
 
 
+// API ROUTES AND BUSINESS LOGIC 
 
 app.get("/", (req, res) => {
   res.redirect("/signup");
 });
 
 app.get("/home", (req, res) => {
-  //make protected
-  res.render("index");
+  if (req.isAuthenticated()){
+    res.render("index");
+  }
+
+  req.flash("error", "Please login to view page");
+  
 });
 
 app
@@ -73,33 +111,15 @@ app
   .get((req, res) => {
     res.render("login");
   })
-  .post(async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      const registeredUser = await User.findOne({ email: email }); //get user by the email
-
-      if (!registeredUser) {
-        req.flash("error", "user not found");
-        res.redirect("/login");
-
-        //  return res.status(401).send("User not found");
-      }
-
-      const match = await bcrypt.compare(password, registeredUser.password);
-
-      if (match) {
-        req.flash("success", "sucecessfully login!");
-        res.redirect("/home");
-      } else {
-        req.flash("error", "failed to login!");
-        res.redirect("/login");
-      }
-    } catch (error) {
-      res.status(500).send("Login error");
-      console.error("login error: ", error);
-    }
-  });
-
+  .post(passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),(req, res) =>{
+    req.flash("success" , "Login successful");
+    res.redirect("/home");
+  }
+);
+   
 app
   .route("/signup")
   .get((req, res) => {
@@ -111,28 +131,24 @@ app
       const { email, password } = req.body;
 
       if (!email || !password) {
-        req.flash("error", "please specify credentials");
-        // res.status(400).send("Please Specify credentials"); // bad request, a flash will be here tho
-        return;
+        return req.flash("error", "please specify credentials"); //check this 
       }
 
-      const hashedPassword = await bcrypt.hash(password, saltRounds);
+      await User.register({email: email} , password);
 
-      await User.create({
-        email: email,
-        password: hashedPassword,
+      passport.authenticate("local")(req, res , () =>{
+        req.flash("success", "successfully signed in");
+        res.redirect("/home");
       });
-      req.flash("success", "successfully signed in");
-      res.redirect("/home");
+      
+
     } catch (err) {
       if (err.code === 11000) {
         req.flash("error", "User already registered");
-        // res.status(500).send("User already registered"); //send and redirect can't work at the same time fix this
         return res.redirect("/signup");
       }
       req.flash("error", "sign up error");
-      // res.status(500).send("Sign up error");
-      console.error("sign up error: ", err);
+      return res.redirect("/signup");
     }
   });
 
