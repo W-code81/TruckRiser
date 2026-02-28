29bff6c8ade6f3fe686882d705bd53b7abddd10d
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-02-28 09:42:50 +0100

    moved dotenv to the top of app

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-require("dotenv").config();
 const validator = require("validator");
 const session = require("express-session");
 const passport = require("passport");
