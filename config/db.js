15be683ced7e357aaafbd08514739fce8c546b39
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-06-29 16:07:05 +0100

    feat: switched to prod url

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // MONGODB INITIALIZATION AND SCHEMA
   const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_LOCAL_URI);
+    await mongoose.connect(process.env.MONGO_ATLAS_URI);
     console.log("Connected to MongoDB Atlas");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err);
